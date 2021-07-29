446e07d082a241a0a68a0fc9c91f223677bc4e2a
Gabriel Martins <gabrielvegaz19@hotmail.com>
2021-07-29 19:15:22 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/anima-scroll.js";
+import initTabNav from "./modules/tabnav.js";
+import initAccordion from "./modules/accordion.js";
+import initFuncionamento from "./modules/funcionamento.js";
+import initTooltip from "./modules/tooltip.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+
+initScrollSuave();
+initAnimacaoScroll();
+initTabNav();
+initAccordion();
+initFuncionamento();
+initTooltip();
+initMenuMobile();
